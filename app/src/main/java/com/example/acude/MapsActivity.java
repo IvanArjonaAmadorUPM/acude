86b08d2fc1800d05cb987929d3672fc0f3d71f47
IvanArjona <ivan.arjona@brooktec.com>
2022-05-17 19:16:11 +0200

    #12 Add routes without actual position

--- app/src/main/java/com/example/acude/MapsActivity.java
+++ app/src/main/java/com/example/acude/MapsActivity.java
@@ -3,21 +3,33 @@ package com.example.acude;
 import androidx.annotation.IdRes;
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
+import android.graphics.Point;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
 import androidx.appcompat.widget.SearchView;
 
 import android.util.Log;
 import android.widget.Toast;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -27,9 +39,11 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.acude.databinding.ActivityMapsBinding;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.model.Place;
 import com.google.android.libraries.places.api.model.RectangularBounds;
@@ -38,7 +52,12 @@ import com.google.android.libraries.places.api.net.PlacesClient;
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -97,8 +116,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
                 getSupportFragmentManager().findFragmentById(R.id.autocomplete_search_fragment);
 
-        assert autocompleteFragment != null;
-        autocompleteFragment.setTypeFilter(TypeFilter.ADDRESS);
+        //assert autocompleteFragment != null;
+        //autocompleteFragment.setTypeFilter(TypeFilter.ADDRESS);
 
         autocompleteFragment.setLocationBias(RectangularBounds.newInstance(
                 new LatLng(40.217127,-3.131993),
@@ -109,12 +128,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
             @Override
             public void onPlaceSelected(@NonNull Place place) {
-                System.out.println("------------------- Place: " + place.getName() + "," + place.getLatLng());
                 mMap.setTrafficEnabled(true);
                 String destinationAddress = place.getName();
                 destinationCoords = place.getLatLng();
                 mMap.addMarker(new MarkerOptions().position(destinationCoords).title(destinationAddress));
                 mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(destinationCoords, 16));
+                setRoute(actualPosition, destinationCoords, mMap);
             }
             @Override
             public void onError(@NonNull Status status) {
@@ -161,25 +180,98 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
-  /*  public void getRoute(LatLng origin, LatLng destination) {
-        try {
-            String url = "https://maps.googleapis.com/maps/api/directions/json?" +
-                    "origin=" + origin.latitude + "," + origin.longitude +
-                    "&" +
-                    "destination="+ destination.latitude + "," + destination.longitude +
-                    "&key=" + "AIzaSyA36f2OoQHWJbcK0QmrQUSUBcAf2F93Lt0";
+    private void setRoute(LatLng actualPosition, LatLng destinationCoords, GoogleMap mMap) {
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        String url = Uri.parse("https://maps.googleapis.com/maps/api/directions/json")
+                .buildUpon()
+                .appendQueryParameter("destination", "40.488571,-3.384761")
+                .appendQueryParameter("origin", "40.486847,-3.375545")
+                .appendQueryParameter("mode", "driving")
+                .appendQueryParameter("key", "AIzaSyCl0SpP6o0pKVJITvTYdhVNSL8o01yDEgk")
+                .appendQueryParameter("traffic_model","optimistic") //best_guess
+                .appendQueryParameter("departure_time","now")
+                .toString();
+        System.out.println("---------" + url);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    String status = response.getString("status");
+                    if (status.equals("OK")) {
+                        JSONArray routes = response.getJSONArray("routes");
+                        ArrayList<LatLng> points;
+                        PolylineOptions polylineOptions = null;
 
-           System.out.println("---------------------- " + url);
-          //  OkHttpClient client = new OkHttpClient().newBuilder()
-                  //  .build();
-          //  Request request = new Request.Builder().url(url)
-                    //.url("https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=YOUR_API_KEY")
-                 //   .method("GET", null)
-                 //   .build();
-           // Response response = client.newCall(request).execute();
-          //  System.out.println("response" + response);
-        } catch (IOException e) {
-            String response = e.toString();
+                        for (int i=0;i<routes.length();i++){
+                            points = new ArrayList<>();
+                            polylineOptions = new PolylineOptions();
+                            JSONArray legs = routes.getJSONObject(i).getJSONArray("legs");
+
+                            for (int j=0;j<legs.length();j++){
+                                JSONArray steps = legs.getJSONObject(j).getJSONArray("steps");
+
+                                for (int k=0;k<steps.length();k++){
+                                    String polyline = steps.getJSONObject(k).getJSONObject("polyline").getString("points");
+                                    List<LatLng> list = decodePoly(polyline);
+
+                                    for (int l=0;l<list.size();l++){
+                                        LatLng position = new LatLng((list.get(l)).latitude, (list.get(l)).longitude);
+                                        points.add(position);
+                                    }
+                                }
+                            }
+                            polylineOptions.addAll(points);
+                            polylineOptions.width(40);
+                            polylineOptions.color(ContextCompat.getColor(MapsActivity.this, R.color.orange));
+                            polylineOptions.geodesic(true);
+                        }
+                        mMap.addPolyline(polylineOptions);
+                        Point point = new Point();
+                        getWindowManager().getDefaultDisplay().getSize(point);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        RetryPolicy retryPolicy = new DefaultRetryPolicy(30000, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+        jsonObjectRequest.setRetryPolicy(retryPolicy);
+        requestQueue.add(jsonObjectRequest);
+    }
+    private List<LatLng> decodePoly(String encoded){
+        List<LatLng> poly = new ArrayList<>();
+        int index = 0, len = encoded.length();
+        int lat = 0, lng = 0;
+
+        while (index < len) {
+            int b, shift = 0, result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lat += dlat;
+
+            shift = 0;
+            result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b > 0x20);
+            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lng += dlng;
+
+            LatLng p = new LatLng((((double) lat / 1E5)),
+                    (((double) lng / 1E5)));
+            poly.add(p);
         }
-    }*/
+        return poly;
+    }
 }
